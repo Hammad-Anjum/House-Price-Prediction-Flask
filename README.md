a5f7520458025c11fd633406335270a214108909
Hammad Anjum <hammad95632@gmail.com>
2024-07-05 05:26:24 +0500

    add styles and run.py

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # House-Price-Prediction-Flask
-The following repository contains a house price prediction (specifically the boston housing dataset) model built and deployed using the Flask API.
+The following repository contains a house price prediction model trained using sklearn and deployed with the help of flask API. The dataset used for this project is the california housing dataset provided in the sklearn.datasets library
 
 ## Dataset
-The dataset consists of : 
-- RM : Average number of rooms per dwelling.
-- LSTAT : Percentage of population considered low status.
-- PTRATIO : Pupil-teacher ratio by town.
-- MEDV : Median value of owner occupied homes (target variable).
+Attribute Information:
+    - MedInc        median income in block group
+    - HouseAge      median house age in block group
+    - AveRooms      average number of rooms per household
+    - AveBedrms     average number of bedrooms per household
+    - Population    block group population
+    - AveOccup      average number of household members
+    - Latitude      block group latitude
+    - Longitude     block group longitude
 
 
 
--- model.py
+++ model.py
@@ -1,63 +1,95 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import pickle
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split , GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error , r2_score , mean_absolute_error
+from feature_engine.outliers import Winsorizer
 
-df = pd.read_csv('housing.csv')
+data = fetch_california_housing(as_frame=True)
+df = data.frame
+Xcols = data.feature_names
 
 print(df)
+print('Features : ' , Xcols)
+print('Target :' , data.target) 
+
+print('Shape of dataframe :' , df.shape)
 
 print(df.info())
 
+print(df.describe())
+
 print(df.isna().sum())
 
-'''
+
 for col in df.columns:
-    sns.histplot(x = col , data = df , kde = True)
+    sns.histplot(x = col , data = df , kde = True , bins = 20)
     plt.title('Histogram of {}'.format(col))
     plt.show()
     plt.figure()
 
 
 for col in df.columns:
-    sns.violinplot(x = col , data = df)
-    plt.title('Voilin Plot of {}'.format(col))
+    sns.boxplot(x = col , data = df)
+    plt.title('Box Plot of {}'.format(col))
     plt.show()
     plt.figure()
-
-for i, col in enumerate(df.columns):
-    for j, col2 in enumerate(df.columns):
-        if j > i and col != col2:
-            sns.scatterplot(x=col, y=col2, data=df, hue="MEDV")
-            plt.title('Scatter Plot: {} vs {}'.format(col , col2))
-            plt.xlabel(col)
-            plt.ylabel(col2)
-            plt.show()
-            plt.figure()
 
 
 sns.heatmap(df.corr() , annot = True, cmap = 'viridis')
 plt.show()
-'''
-X = df.drop('MEDV' , axis = 1)
-y = df['MEDV']
+
+winsorizer =  Winsorizer(capping_method= 'iqr' , tail = 'both' , fold = 3)
+
+df[Xcols] = winsorizer.fit_transform(df[Xcols])
+
+for col in df.columns:
+    if col != 'MedHouseVal':
+        sns.histplot(x = col , data = df , kde = True , bins = 20)
+        plt.title('Histogram of {} after winsorizing'.format(col))
+        plt.show()
+        plt.figure()
+
+
+X = df.drop('MedHouseVal' , axis = 1)
+y = df['MedHouseVal']
 
 X_train , X_test , y_train , y_test = train_test_split(X , y , shuffle=True , random_state=42 , test_size= 0.2)
 
 model = RandomForestRegressor()
 
-model.fit(X_train , y_train)
-
-y_pred = model.predict(X_test)
-
-df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-
-sns.lmplot(x = 'Actual',y = 'Predicted' , data = df)
-
-plt.title('Regression plot with actual vs predicted values')
-plt.show()
+param_grid = {
+    'n_estimators': [50, 100, 200],   
+    'max_depth': [None, 10, 20],      
+    'min_samples_split': [2, 5, 10],  
+    'min_samples_leaf': [1, 2, 4],    
+    'max_features': ['auto', 'sqrt']  
+}
 
 
+gscv = GridSearchCV(model , param_grid , cv = 3 , verbose=2)
 
+gscv.fit(X_train , y_train)
+
+print(gscv.best_estimator_)
+
+#best estimator was 
+#RandomForestRegressor(max_features='sqrt', max_depth = 20,n_estimators=200)
+bestmodel = gscv.best_estimator_
+
+bestmodel.fit(X_train , y_train)
+
+y_pred = bestmodel.predict(X_test)
+
+print("R square : " , r2_score(y_test , y_pred))
+
+print('Mean squared error : ' , mean_squared_error(y_test , y_pred))
+
+print('Mean absolute error : ' , mean_absolute_error(y_test , y_pred))
+
+
+pickle.dump(bestmodel, open('model.pkl', 'wb'))
